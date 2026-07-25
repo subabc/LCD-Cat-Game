73271b97caa6b357b46daa4c193fb0f4e3fb4db3
Chloe <sutimari01@gmail.com>
2026-07-25 18:49:16 +0100

    created functions before doing score counter

--- catGame.ino
+++ catGame.ino
@@ -28,12 +28,12 @@ byte catman[8] = {
 
 byte obstacle[8] = {
   B00100,
-  B01110,
-  B01110,
-  B11111,
-  B11111,
-  B01110,
-  B01110,
+  B00101,
+  B10101,
+  B10111,
+  B11100,
+  B00100,
+  B00100,
   B00100
 };
 
@@ -57,6 +57,14 @@ const int jumpTime = 500; //jump duration -- ended up increading jump time so ca
 unsigned long lastMoveTime = 0;
 const int obsSpeed = 300;
 
+void initSprites(){
+  lcd.setCursor(0, catRow); //initilise character to bottom row
+  lcd.write(byte(0)); //prints the character out at position 0
+
+  lcd.setCursor(obsCol, 1);
+  lcd.write(byte(1));
+}
+
 void setup() {
   lcd.init();
   lcd.backlight();
@@ -66,20 +74,12 @@ void setup() {
 
   pinMode(button, INPUT_PULLUP); //INTPUT_PULLUP is the resistor that changes the voltage to determine high/low voltage movement on board 
   //high resistance = slows current --> no movement, low resistance = increases current --> movement
-  
-  lcd.setCursor(0, catRow); //initilise character to bottom row
-  lcd.write(byte(0)); //prints the character out at position 0
-
-
-  lcd.setCursor(obsCol, 1);
-  lcd.write(byte(1));
-
-  // lcd.clear();//clear old data when program is initilised
+  initSprites();
 }
 
-void loop() {
-  // lcd.clear(); //need to clear screen to change character position 
-
+// Function to jump up
+void up(){ 
+  
   if (digitalRead(button) == LOW && !didJump){ //if button is pressed then
 
     delay(50); //need to add delay as there was an issue with the objects coliding
@@ -98,12 +98,15 @@ void loop() {
     lcd.setCursor(0, catRow);//print char
     lcd.write(byte(0));
 
-
-
-
   }// do not need to include else if as player can spam the button so the char stays on the top row to avoid obstacles
 
-//when cat has jumped, put it back on row 1 
+}
+
+
+// Function for landing back down
+void down(){
+
+  //when cat has jumped, put it back on row 1 
   if (didJump && millis() - jumpStartTime > jumpTime){
     //if the char did not jump and the (duration of the jump - start jump time) > jump time, then the current is high, which means that the jump is over 
     // therefore, clear position 
@@ -115,12 +118,11 @@ void loop() {
       lcd.write(byte(0));
 
       didJump = false; // initilise jump condition again 
-
-
-
   }
+}
 
-// moving obstacle on screen
+void moveObst(){
+  // moving obstacle on screen
   if (millis() - lastMoveTime > obsSpeed){
     lastMoveTime = millis();
 
@@ -136,14 +138,12 @@ void loop() {
       lcd.setCursor(obsCol, obsRow);//print it out to new position
       lcd.write(byte(1));
   }
+}
 
-
-      //detecting collision
-      if (obsCol == 0 && catRow == obsRow){
-    
+void gameOver(){
         lcd.clear();
-        lcd.setCursor(5, 0);
-        lcd.print("RIP...");
+        lcd.setCursor(3, 0);
+        lcd.print("GAME OVER!");
         lcd.setCursor(4, 1);
         lcd.print("Restart?");
 
@@ -161,17 +161,21 @@ void loop() {
         jumpStartTime = 0;
         lastMoveTime = millis();
 
-
         lcd.clear();
+        initSprites();
+      
+}
 
+void detectCollision(){
+        //detecting collision
+      if (obsCol == 0 && catRow == obsRow){
+        gameOver();
+    }
+}
 
-        lcd.setCursor(0, catRow);
-        lcd.write(byte(0));
-
-
-        lcd.setCursor(obsCol, obsRow);
-        lcd.write(byte(1));
-
-      }
-
+void loop() {
+  up();
+  down();
+  moveObst();
+  detectCollision();
 }
