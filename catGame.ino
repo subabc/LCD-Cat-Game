57cb04d198aba8400ea4f8198ff0e40fe0e34fe2
Chloe <sutimari01@gmail.com>
2026-07-25 15:27:52 +0100

    added collision detection

--- catGame.ino
+++ catGame.ino
@@ -39,13 +39,20 @@ byte obstacle[8] = {
 
 const int button = 2; //pin connected to 2 on board 
 
-int charPos = 1; //start position at the bottom row --> top row == 0
-int obsPos = 15; //place at the end of the screen 
+int catRow = 1; //start position at the bottom row --> top row == 0
+int obsCol = 15; //place at the end of the screen 
+
+int obsRow = 1; //obstacle alwys on bottom row 
+
+// Display ref:
+// Columns: 0 1 2 3 ... 15
+// Row 0:
+// Row 1:  
 
 bool didJump = false; //default to bottom row when button is not pressed 
 
 unsigned long jumpStartTime; //storing the exact time the character jumps
-const int jumpTime = 400; //jump duration 
+const int jumpTime = 500; //jump duration -- ended up increading jump time so cat stays in the top row for longer as there was an issue with collison object
 
 unsigned long lastMoveTime = 0;
 const int obsSpeed = 300;
@@ -60,11 +67,11 @@ void setup() {
   pinMode(button, INPUT_PULLUP); //INTPUT_PULLUP is the resistor that changes the voltage to determine high/low voltage movement on board 
   //high resistance = slows current --> no movement, low resistance = increases current --> movement
   
-  lcd.setCursor(0, charPos); //initilise character to bottom row
+  lcd.setCursor(0, catRow); //initilise character to bottom row
   lcd.write(byte(0)); //prints the character out at position 0
 
 
-  lcd.setCursor(obsPos, 1);
+  lcd.setCursor(obsCol, 1);
   lcd.write(byte(1));
 
   // lcd.clear();//clear old data when program is initilised
@@ -74,15 +81,21 @@ void loop() {
   // lcd.clear(); //need to clear screen to change character position 
 
   if (digitalRead(button) == LOW && !didJump){ //if button is pressed then
+
+    delay(50); //need to add delay as there was an issue with the objects coliding
+    // cat disappeared sometimes when it jumped over the object and returned back in the same cell
+    //but then returned when button was pressed 
+
+    // could have sped up the collisison object speed but that will not fix issues in the future for game speed mode
     didJump = true;
     jumpStartTime = millis(); // save start time to compare it to when player stops pressing button
 
-    lcd.setCursor(0, charPos); //erase the character at it's current position
+    lcd.setCursor(0, catRow); //erase the character at it's current position
     lcd.print(" ");
 
-    charPos = 0; // move char to the top row
+    catRow = 0; // move char to the top row
 
-    lcd.setCursor(0, charPos);//print char
+    lcd.setCursor(0, catRow);//print char
     lcd.write(byte(0));
 
 
@@ -90,14 +103,15 @@ void loop() {
 
   }// do not need to include else if as player can spam the button so the char stays on the top row to avoid obstacles
 
+//when cat has jumped, put it back on row 1 
   if (didJump && millis() - jumpStartTime > jumpTime){
     //if the char did not jump and the (duration of the jump - start jump time) > jump time, then the current is high, which means that the jump is over 
     // therefore, clear position 
-      lcd.setCursor(0, charPos);
+      lcd.setCursor(0, catRow);
       lcd.print(" ");
 
-      charPos = 1; // put character back to bottom row
-      lcd.setCursor(0, charPos);
+      catRow = 1; // put character back to bottom row
+      lcd.setCursor(0, catRow);
       lcd.write(byte(0));
 
       didJump = false; // initilise jump condition again 
@@ -106,54 +120,58 @@ void loop() {
 
   }
 
-
+// moving obstacle on screen
   if (millis() - lastMoveTime > obsSpeed){
     lastMoveTime = millis();
 
-      lcd.setCursor(obsPos, 1); //clear current position of obstacle on the bottom row
+      lcd.setCursor(obsCol, obsRow); //clear current position of obstacle on the bottom row
       lcd.print(" ");
 
-      obsPos--; //decrement position of obstacle --> move to the left
+      obsCol--; //decrement position of obstacle --> move to the left
 
-      if (obsPos < 0){//ensure that the obstacle doesn't leave the screen 
-        obsPos = 15; //take it back to beginning position
+      if (obsCol < 0){//ensure that the obstacle doesn't leave the screen 
+        obsCol = 15; //take it back to beginning position
       }
 
-      lcd.setCursor(obsPos, 1);//print it out to new position
+      lcd.setCursor(obsCol, obsRow);//print it out to new position
       lcd.write(byte(1));
   }
 
-  // lcd.setCursor(charPos, 0);
-  // lcd.write(byte(0));
 
-  // charPos++;//increment character by one space
+      //detecting collision
+      if (obsCol == 0 && catRow == obsRow){
+    
+        lcd.clear();
+        lcd.setCursor(5, 0);
+        lcd.print("RIP...");
+        lcd.setCursor(4, 1);
+        lcd.print("Restart?");
 
-  // if (charPos > 15){
-  //   charPos = 0; // set character back to starting pos if it goes to the end of the display
+        while (digitalRead(button) == HIGH){//end game
+        }
 
+        delay(200);
+        
+        //reset position variables
+        catRow = 1;
+        obsCol = 15;
+        obsRow = 1;
+        didJump = false;
 
-  // }
-
-  // delay(300);
-
-
-
-
-
-
-
-
-
-
-
-
-
+        jumpStartTime = 0;
+        lastMoveTime = millis();
 
 
+        lcd.clear();
 
 
+        lcd.setCursor(0, catRow);
+        lcd.write(byte(0));
 
 
+        lcd.setCursor(obsCol, obsRow);
+        lcd.write(byte(1));
 
+      }
 
 }
